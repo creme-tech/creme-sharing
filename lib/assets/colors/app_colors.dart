243fb6ba633704aa284e1efd5a566930cfdb4ddc
pedrohsampaioo <pedrohsampaioo@outlook.com>
2022-07-21 15:07:26 -0300

    chore: add creme colors

---
+++ lib/assets/colors/app_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const white10 = Color.fromRGBO(255, 255, 255, 0.1);
+  static const white20 = Color.fromRGBO(255, 255, 255, 0.2);
+  static const white50 = Color.fromRGBO(255, 255, 255, 0.5);
+  static const white60 = Color.fromRGBO(255, 255, 255, 0.6);
+  static const white70 = Color.fromRGBO(255, 255, 255, 0.7);
+  static const white80 = Color.fromRGBO(255, 255, 255, 0.8);
+  static const black10 = Color.fromRGBO(0, 0, 0, 0.1);
+  static const black20 = Color.fromRGBO(0, 0, 0, 0.2);
+  static const black50 = Color.fromRGBO(0, 0, 0, 0.5);
+  static const black70 = Color.fromRGBO(0, 0, 0, 0.7);
+  static const grayscale = Color(0xFF0E0E0E);
+  static const gray = Color(0xFF161616);
+  static const lightGray = Color(0xFFF3F3F3);
+  static const gray20 = Color(0xFF4B4B4C);
+  static const backgroundWebViewModal = Color(0xFF565757);
+  static const webViewUrlColor = Color(0xFF8E8E8E);
+  static const webViewTextButtonColor = Color(0xFF3771E0);
+  static const red = Color(0xFFE0322C);
+  static const red1 = Color(0xFFFF3F00);
+  static const red2 = Color(0xFFD3281F);
+  static const red3 = Color(0xFFED4956);
+  static const green = Color(0xFF93C36B);
+  static const avatarBackgroundColor = Colors.amber;
+}
