707d48bdb7bd79ccebf49ad073d7f9828d29782b
pedrohsampaioo <pedrohsampaioo@outlook.com>
2022-07-21 15:11:52 -0300

    feat: create method shareCreatorToInstagramStories on CremeSharing

--- lib/creme_sharing.dart
+++ lib/creme_sharing.dart
@@ -1,5 +1,10 @@
-import 'dart:ui';
+import 'dart:convert';
+import 'package:flutter/services.dart';
 
+import 'package:flutter/material.dart';
+import 'package:screenshot/screenshot.dart';
+
+import 'package:creme_sharing/utils/creator_sticker_widget.dart';
 import 'creme_sharing_platform_interface.dart';
 
 class CremeSharing {
@@ -20,6 +25,40 @@ class CremeSharing {
       backgroundVideo: backgroundVideo,
       backgroundImage: backgroundImage,
       contentURL: contentURL,
+    );
+  }
+
+  Future<void> shareCreatorToInstagramStories({
+    required String creatorAvatarUrl,
+    required String creatorName,
+    required String creatorTag,
+    required String cremeLogoMessage,
+    required BuildContext context,
+    Color backgroundColor = Colors.grey,
+    String? contentURL,
+  }) async {
+    final devicePixelRatio = MediaQuery.of(context).devicePixelRatio;
+    final creatorAvatarImage =
+        (await NetworkAssetBundle(Uri.parse(creatorAvatarUrl)).load(''))
+            .buffer
+            .asUint8List();
+    final stickerImagePngBytes = await ScreenshotController().captureFromWidget(
+      CreatorStickerWidget(
+        backgroundColor: backgroundColor,
+        creatorAvatarImage: creatorAvatarImage,
+        creatorName: creatorName,
+        creatorTag: creatorTag,
+        cremeLogoMessage: cremeLogoMessage,
+      ),
+      pixelRatio: devicePixelRatio,
+      context: context,
+    );
+    final stickerImageBase64 = base64Encode(stickerImagePngBytes);
+    await shareToInstagramStories(
+      backgroundImage: stickerImageBase64,
+      backgroundTopColor: backgroundColor,
+      backgroundBottomColor: backgroundColor,
+      // stickerImage: stickerImageBase64,
     );
   }
 }
