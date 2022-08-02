912cb0cdb6ad7351bfda5a7a66d7c0ca399bd08f
pedrohsampaioo <pedrohsampaioo@outlook.com>
2022-08-02 17:10:57 -0300

    feat: create example screen

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,69 +1,278 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:creme_sharing/creme_sharing.dart';
+import 'package:creme_sharing/utils/recipe_sticker_widget.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _cremeSharingPlugin = CremeSharing();
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late CremeSharing cremeSharing;
+  final backgroundVideoUrl =
+      'https://storage.googleapis.com/muxdemofiles/mux-video-intro.mp4';
+  bool generateWithBackgroundVideo = false;
+  bool? instagramIsAvailableToShare;
+  bool loading = false;
+  Duration? durationToShare;
 
   @override
   void initState() {
+    cremeSharing = CremeSharing();
+    cremeSharing.instagramIsAvailableToShare().then(
+          (value) => WidgetsBinding.instance.addPostFrameCallback((_) {
+            setState(() {
+              instagramIsAvailableToShare = value;
+            });
+          }),
+        );
     super.initState();
-    initPlatformState();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      _cremeSharingPlugin.shareToInstagramStories(
-        backgroundImage:
-            'https://images.ctfassets.net/4kdyp3e5mlne/2n7azZ2QJjY5f937HtdZQL/10d6e5de71721348185ac4df0973e1d4/Marie.jpg',
-        backgroundTopColor: Colors.red,
-        backgroundBottomColor: Colors.orange,
-        stickerImage:
-            'https://images.ctfassets.net/4kdyp3e5mlne/1XGsZB6hhvceAFM241dqGW/1fad3eab1a652a5276b5e0f55afea5e8/ThaiBasilGuacamoleImage.png',
-      );
-      platformVersion = 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+  @override
+  void dispose() {
+    cremeSharing.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
+    final safeAreaPadding = MediaQuery.of(context).padding;
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor: Colors.white,
+      body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          if (instagramIsAvailableToShare == null || loading)
+            const SliverFillRemaining(
+              child: Center(child: CircularProgressIndicator()),
+            ),
+          if (instagramIsAvailableToShare != null &&
+              !instagramIsAvailableToShare! &&
+              !loading)
+            const SliverFillRemaining(
+              child: Center(child: Text('Instagram is not available to share')),
+            ),
+          if ((instagramIsAvailableToShare ?? false) && !loading)
+            SliverToBoxAdapter(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(height: safeAreaPadding.top + 24),
+                  if (durationToShare != null)
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Text(
+                          'took ${durationToShare?.abs().toString()} to share in the last interection'),
+                    ),
+                  if (durationToShare != null) const SizedBox(height: 24),
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        final initialDateTime = DateTime.now();
+                        WidgetsBinding.instance.addPostFrameCallback((_) {
+                          setState(() {
+                            loading = true;
+                          });
+                        });
+                        await cremeSharing.shareCreatorToInstagramStories(
+                          backgroundVideoUrl: generateWithBackgroundVideo
+                              ? backgroundVideoUrl
+                              : null,
+                          creatorAvatarUrl:
+                              'https://images.ctfassets.net/3p29fwo3qudu/40EFY4w8CnahIpQUtuRqlH/77e42391bc90af801454acccb8da1857/creme-marcio-silva-30-output.jpg',
+                          creatorName: 'Márcio Silva',
+                          creatorTag: 'Mixologist',
+                          cremeLogoMessage: 'Just joined',
+                          context: context,
+                        );
+                        WidgetsBinding.instance.addPostFrameCallback((_) {
+                          setState(() {
+                            loading = false;
+                            durationToShare =
+                                initialDateTime.difference(DateTime.now());
+                          });
+                        });
+                      },
+                      child: const Text('share creator to instagram stories'),
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        final initialDateTime = DateTime.now();
+                        WidgetsBinding.instance.addPostFrameCallback((_) {
+                          setState(() {
+                            loading = true;
+                          });
+                        });
+                        await cremeSharing.shareRecipesToInstagramStories(
+                          extraRecipesToShow: [],
+                          backgroundVideoUrl: generateWithBackgroundVideo
+                              ? backgroundVideoUrl
+                              : null,
+                          recipeImageUrl:
+                              'https://images.ctfassets.net/3p29fwo3qudu/6FeBEw8yYlcOwxnCYemQey/5c526b3e4d13e9373678cb759e71d9ba/creme-marcio-silva-1-output.jpg',
+                          creatorAvatarUrl:
+                              'https://images.ctfassets.net/3p29fwo3qudu/40EFY4w8CnahIpQUtuRqlH/77e42391bc90af801454acccb8da1857/creme-marcio-silva-30-output.jpg',
+                          creatorName: 'Márcio Silva',
+                          recipeName: 'Simple Syrup',
+                          cremeLogoMessage: 'Cook with',
+                          context: context,
+                        );
+                        WidgetsBinding.instance.addPostFrameCallback((_) {
+                          setState(() {
+                            loading = false;
+
+                            durationToShare =
+                                initialDateTime.difference(DateTime.now());
+                          });
+                        });
+                      },
+                      child: const Text('share 1 recipe to instagram stories'),
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        final initialDateTime = DateTime.now();
+                        WidgetsBinding.instance.addPostFrameCallback((_) {
+                          setState(() {
+                            loading = true;
+                          });
+                        });
+                        await cremeSharing.shareRecipesToInstagramStories(
+                          extraRecipesToShow: [
+                            const RecipeData(
+                              recipeName: 'Simple Syrup',
+                              recipeImageUrl:
+                                  'https://images.ctfassets.net/3p29fwo3qudu/6FeBEw8yYlcOwxnCYemQey/5c526b3e4d13e9373678cb759e71d9ba/creme-marcio-silva-1-output.jpg',
+                              recipeImageFile: null,
+                            ),
+                            const RecipeData(
+                              recipeName: 'Simple Syrup',
+                              recipeImageUrl:
+                                  'https://images.ctfassets.net/3p29fwo3qudu/6FeBEw8yYlcOwxnCYemQey/5c526b3e4d13e9373678cb759e71d9ba/creme-marcio-silva-1-output.jpg',
+                              recipeImageFile: null,
+                            ),
+                          ],
+                          backgroundVideoUrl: generateWithBackgroundVideo
+                              ? backgroundVideoUrl
+                              : null,
+                          recipeImageUrl:
+                              'https://images.ctfassets.net/3p29fwo3qudu/6FeBEw8yYlcOwxnCYemQey/5c526b3e4d13e9373678cb759e71d9ba/creme-marcio-silva-1-output.jpg',
+                          creatorAvatarUrl:
+                              'https://images.ctfassets.net/3p29fwo3qudu/40EFY4w8CnahIpQUtuRqlH/77e42391bc90af801454acccb8da1857/creme-marcio-silva-30-output.jpg',
+                          creatorName: 'Márcio Silva',
+                          recipeName: 'Simple Syrup',
+                          cremeLogoMessage: 'Cook with',
+                          context: context,
+                        );
+                        WidgetsBinding.instance.addPostFrameCallback((_) {
+                          setState(() {
+                            loading = false;
+
+                            durationToShare =
+                                initialDateTime.difference(DateTime.now());
+                          });
+                        });
+                      },
+                      child: const Text('share 3 recipes to instagram stories'),
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        final initialDateTime = DateTime.now();
+                        WidgetsBinding.instance.addPostFrameCallback((_) {
+                          setState(() {
+                            loading = true;
+                          });
+                        });
+                        await cremeSharing.shareCookedToInstagramStories(
+                          backgroundVideoUrl: generateWithBackgroundVideo
+                              ? backgroundVideoUrl
+                              : null,
+                          userName: 'Fe de Lamare',
+                          userAvatarUrl:
+                              'https://cdn.cre.me/avatar/b5d90c37-2d66-49b5-a148-51006bdb2f7f.jpg',
+                          cookedImageUrl:
+                              'https://cdn.cre.me/share/1141003d-7c9d-4b71-b280-e16a13eba904.jpg',
+                          creatorAvatarUrl:
+                              'https://downloads.ctfassets.net/3p29fwo3qudu/7HxtqQ26sWwCyZHZSoYDpF/771125e358cce423da4384ae486badcf/040A0810.jpeg',
+                          creatorName: 'Astor Bar',
+                          recipeName: 'Smoked Salmon Club Sandwich',
+                          cremeLogoMessage: 'Cooked with',
+                          context: context,
+                        );
+                        WidgetsBinding.instance.addPostFrameCallback((_) {
+                          setState(() {
+                            loading = false;
+                            durationToShare =
+                                initialDateTime.difference(DateTime.now());
+                          });
+                        });
+                      },
+                      child: const Text('share cooked to instagram stories'),
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        CupertinoSwitch(
+                          value: generateWithBackgroundVideo,
+                          onChanged: (value) {
+                            WidgetsBinding.instance.addPostFrameCallback((_) {
+                              setState(() {
+                                generateWithBackgroundVideo = value;
+                              });
+                            });
+                          },
+                        ),
+                        const SizedBox(width: 4),
+                        const Text('Generate with Background video'),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: safeAreaPadding.bottom + 24)
+                ],
+              ),
+            ),
+        ],
       ),
     );
   }
