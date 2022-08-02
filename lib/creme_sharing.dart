5c9eaa062c6aeea622cd40be5715044fc8e98f7c
pedrohsampaioo <pedrohsampaioo@outlook.com>
2022-08-02 16:16:19 -0300

    feat: update all share methods to instagram to avoid memory leak and to design be equals of figma

--- lib/creme_sharing.dart
+++ lib/creme_sharing.dart
@@ -1,15 +1,28 @@
+import 'dart:math';
 import 'dart:convert';
-import 'package:flutter/services.dart';
+import 'dart:io' show HttpClient, File;
 
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:screenshot/screenshot.dart';
+import 'package:flutter/material.dart';
 
 import 'package:creme_sharing/utils/creator_sticker_widget.dart';
 import 'package:creme_sharing/creme_sharing_platform_interface.dart';
+import 'package:creme_sharing/utils/cooked_sticker_widget.dart';
 import 'package:creme_sharing/utils/recipe_sticker_widget.dart';
 
 class CremeSharing {
-  const CremeSharing();
+  final ScreenshotController _screenshotController;
+  final HttpClient _httpClient;
+
+  CremeSharing()
+      : _httpClient = HttpClient(),
+        _screenshotController = ScreenshotController();
+
+  Future<void> dispose() async {
+    _httpClient.close(force: true);
+  }
 
   Future<void> shareToInstagramStories({
     Color? backgroundTopColor,
@@ -29,75 +42,329 @@ class CremeSharing {
     );
   }
 
+  /// This method should be used to share some creator to Instagram stories
+  /// and the mockup can be found on: https://www.figma.com/file/r5ox3y5gRNFXPDb1KcsQS3/CREME-2022?node-id=2316%3A217770
+  /// Then it will create an image and othe background will be the [backgroundVideoUrl]
+  /// if that's not null else will be render [creatorAvatarUrl] as background image.
   Future<void> shareCreatorToInstagramStories({
     required String creatorAvatarUrl,
     required String creatorName,
-    required String creatorTag,
     required String cremeLogoMessage,
+    required String creatorTag,
     required BuildContext context,
+    required String? backgroundVideoUrl,
     Color backgroundColor = Colors.grey,
+    double textScaleFactor = 1.2,
     String? contentURL,
   }) async {
-    final devicePixelRatio = MediaQuery.of(context).devicePixelRatio;
-    final creatorAvatarImage =
-        (await NetworkAssetBundle(Uri.parse(creatorAvatarUrl)).load(''))
-            .buffer
-            .asUint8List();
-    final stickerImagePngBytes = await ScreenshotController().captureFromWidget(
+    final devicePixelRatio =
+        max(3, MediaQuery.of(context).devicePixelRatio).toDouble();
+    final screenSize = MediaQuery.of(context).size;
+    final safeAreaPadding = MediaQuery.of(context).padding;
+    final maxHeight = screenSize.height - safeAreaPadding.vertical - 48;
+    final maxWidth = screenSize.width;
+    const avatarSize = Size(40, 40);
+    const extraRecipesImageSize = Size(108, 155);
+    final imageBackgroundSize = Size(maxWidth, maxHeight);
+    final hasVideoOnBackground = backgroundVideoUrl != null;
+    final imageBackgroundUrlToDownload = '$creatorAvatarUrl'
+        '?w=${(devicePixelRatio * imageBackgroundSize.width).round()}'
+        '&h=${(devicePixelRatio * imageBackgroundSize.width).round()}';
+    final creatorAvatarUrlToDownload = '$creatorAvatarUrl'
+        '?w=${(devicePixelRatio * avatarSize.width).round()}'
+        '&h=${(devicePixelRatio * avatarSize.width).round()}';
+    final files = await Future.wait([
+      _downloadFile(
+        url: creatorAvatarUrlToDownload,
+        filename: 'creator_avatar.png',
+      ),
+      if (!hasVideoOnBackground)
+        _downloadFile(
+          url: imageBackgroundUrlToDownload,
+          filename: 'image_background.png',
+        ),
+    ]);
+    final stickerImagePngBytes = await _screenshotController.captureFromWidget(
       CreatorStickerWidget(
-        backgroundColor: backgroundColor,
-        creatorAvatarImage: creatorAvatarImage,
-        creatorName: creatorName,
+        extraRecipesImageSize: extraRecipesImageSize,
+        textScaleFactor: textScaleFactor,
+        hasVideoOnBackground: hasVideoOnBackground,
+        creatorAvatarFile: files[0]!,
+        imageBackgroundFile: files.length >= 2 ? files.last : null,
+        imageBackgroundSize: imageBackgroundSize,
+        creatorAvatarSize: avatarSize,
         creatorTag: creatorTag,
+        backgroundColor: backgroundColor,
+        creatorName: creatorName,
         cremeLogoMessage: cremeLogoMessage,
+        devicePixelRatio: devicePixelRatio,
       ),
       pixelRatio: devicePixelRatio,
       context: context,
     );
-    final stickerImageBase64 = base64Encode(stickerImagePngBytes);
-    await shareToInstagramStories(
-      backgroundImage: stickerImageBase64,
-      backgroundTopColor: backgroundColor,
-      backgroundBottomColor: backgroundColor,
-      // stickerImage: stickerImageBase64,
-    );
+    await Future.wait([
+      ...files.map<Future>((file) async {
+        final exists = (await file?.exists()) ?? false;
+        if (exists) {
+          await file?.delete();
+        }
+      }).toList(),
+      shareToInstagramStories(
+        backgroundImage:
+            hasVideoOnBackground ? null : base64Encode(stickerImagePngBytes),
+        stickerImage:
+            hasVideoOnBackground ? base64Encode(stickerImagePngBytes) : null,
+        backgroundTopColor: backgroundColor,
+        backgroundBottomColor: backgroundColor,
+        contentURL: contentURL,
+        backgroundVideo: backgroundVideoUrl,
+      ),
+    ]);
   }
 
-  Future<void> shareRecipeToInstagramStories({
+  /// This method should be used to share some recipe to Instagram stories
+  /// and the mockup can be found on:
+  /// 1. one recipe : https://www.figma.com/file/r5ox3y5gRNFXPDb1KcsQS3/CREME-2022?node-id=2316%3A217803;
+  /// 2. three recipes : https://www.figma.com/file/r5ox3y5gRNFXPDb1KcsQS3/CREME-2022?node-id=2316%3A217730;
+  /// Then it will create an image and othe background will be the [backgroundVideoUrl]
+  /// if that's not null else will be render [recipeImageUrl] as background image.
+  /// You should provide the [recipeImageUrl] that will positioned in the center and the
+  /// [extraRecipesToShow] will be positioned on the left and right.
+  Future<void> shareRecipesToInstagramStories({
+    required String creatorAvatarUrl,
+    required String recipeImageUrl,
     required String creatorName,
     required String recipeName,
-    required String recipeImageUrl,
     required String cremeLogoMessage,
-    required String creatorAvatarUrl,
     required BuildContext context,
+    required String? backgroundVideoUrl,
+    required List<RecipeData> extraRecipesToShow,
     Color backgroundColor = Colors.grey,
+    double textScaleFactor = 1.2,
     String? contentURL,
   }) async {
-    final devicePixelRatio = MediaQuery.of(context).devicePixelRatio;
-    final imagesResult = await Future.wait([
-      (NetworkAssetBundle(Uri.parse(creatorAvatarUrl)).load('')),
-      (NetworkAssetBundle(Uri.parse(recipeImageUrl)).load('')),
+    final devicePixelRatio =
+        max(3, MediaQuery.of(context).devicePixelRatio).toDouble();
+    final screenSize = MediaQuery.of(context).size;
+    final safeAreaPadding = MediaQuery.of(context).padding;
+    final maxHeight = screenSize.height - safeAreaPadding.vertical - 48;
+    final maxWidth = screenSize.width;
+    const avatarSize = Size(40, 40);
+    final recipeImageSize = extraRecipesToShow.length < 2
+        ? const Size(261, 261)
+        : const Size(165, 260);
+    const extraRecipesImageSize = Size(108, 155);
+    final imageBackgroundSize = Size(maxWidth, maxHeight);
+    final hasVideoOnBackground = backgroundVideoUrl != null;
+    final imageBackgroundUrlToDownload = '$recipeImageUrl'
+        '?w=${(devicePixelRatio * imageBackgroundSize.width).round()}'
+        '&h=${(devicePixelRatio * imageBackgroundSize.width).round()}';
+    final creatorAvatarUrlToDownload = '$creatorAvatarUrl'
+        '?w=${(devicePixelRatio * avatarSize.width).round()}'
+        '&h=${(devicePixelRatio * avatarSize.width).round()}';
+    final recipeImageUrlToDownload = '$recipeImageUrl'
+        '?w=${(devicePixelRatio * recipeImageSize.width).round()}'
+        '&h=${(devicePixelRatio * recipeImageSize.width).round()}';
+    final files = await Future.wait([
+      _downloadFile(
+        url: creatorAvatarUrlToDownload,
+        filename: 'creator_avatar.png',
+      ),
+      _downloadFile(
+        url: recipeImageUrlToDownload,
+        filename: 'recipe_image.png',
+      ),
+      if (!hasVideoOnBackground)
+        _downloadFile(
+          url: imageBackgroundUrlToDownload,
+          filename: 'image_background.png',
+        ),
+      ...List.of(extraRecipesToShow)
+          .take(2)
+          .map<Future>((recipeData) => _downloadFile(
+                url: '${recipeData.recipeImageUrl}'
+                    '?w=${(devicePixelRatio * extraRecipesImageSize.width).round()}'
+                    '&h=${(devicePixelRatio * extraRecipesImageSize.width).round()}',
+                filename:
+                    '${recipeData.recipeName.trim().toLowerCase().replaceAll(' ', '_')}.png',
+              ).then(
+                (value) {
+                  if (value != null) {
+                    final index = extraRecipesToShow.indexOf(recipeData);
+                    extraRecipesToShow[index] = extraRecipesToShow[index]
+                        .copyWith(recipeImageFile: value);
+                  }
+                },
+              ))
+          .toList(),
     ]);
-    final creatorAvatarImage = imagesResult[0].buffer.asUint8List();
-    final recipeImage = imagesResult[1].buffer.asUint8List();
-    final stickerImagePngBytes = await ScreenshotController().captureFromWidget(
+    final stickerImagePngBytes = await _screenshotController.captureFromWidget(
       RecipeStickerWidget(
-        recipeImage: recipeImage,
+        extraRecipesImageSize: extraRecipesImageSize,
+        textScaleFactor: textScaleFactor,
+        hasVideoOnBackground: hasVideoOnBackground,
+        creatorAvatarFile: files[0]!,
+        recipeImageFile: files[1]!,
+        imageBackgroundFile: files.length >= 3 ? files[2] : null,
+        extraRecipesToShow: List.of(extraRecipesToShow)
+            .where((recipeData) => recipeData.recipeImageFile != null)
+            .take(2)
+            .toList(),
+        recipeImageSize: recipeImageSize,
+        imageBackgroundSize: imageBackgroundSize,
+        creatorAvatarSize: avatarSize,
         recipeName: recipeName,
         backgroundColor: backgroundColor,
-        creatorAvatarImage: creatorAvatarImage,
         creatorName: creatorName,
         cremeLogoMessage: cremeLogoMessage,
+        devicePixelRatio: devicePixelRatio,
       ),
       pixelRatio: devicePixelRatio,
       context: context,
     );
-    final stickerImageBase64 = base64Encode(stickerImagePngBytes);
-    await shareToInstagramStories(
-      backgroundImage: stickerImageBase64,
-      backgroundTopColor: backgroundColor,
-      backgroundBottomColor: backgroundColor,
-      // stickerImage: stickerImageBase64,
+    await Future.wait([
+      ...files.map<Future>((file) async {
+        final exists = (await file?.exists()) ?? false;
+        if (exists) {
+          await file?.delete();
+        }
+      }).toList(),
+      shareToInstagramStories(
+        backgroundImage:
+            hasVideoOnBackground ? null : base64Encode(stickerImagePngBytes),
+        stickerImage:
+            hasVideoOnBackground ? base64Encode(stickerImagePngBytes) : null,
+        backgroundTopColor: backgroundColor,
+        backgroundBottomColor: backgroundColor,
+        contentURL: contentURL,
+        backgroundVideo: backgroundVideoUrl,
+      ),
+    ]);
+  }
+
+  /// This method should be used to share some cooked recipe to Instagram stories
+  /// and the mockup can be found on: https://www.figma.com/file/r5ox3y5gRNFXPDb1KcsQS3/CREME-2022?node-id=2316%3A217838
+  ///
+  /// Then it will create an image and othe background will be the [backgroundVideoUrl]
+  /// if that's not null else will be render [creatorAvatarUrl] as background image
+  Future<void> shareCookedToInstagramStories({
+    required String userName,
+    required String? userAvatarUrl,
+    required String cookedImageUrl,
+    required String creatorAvatarUrl,
+    required String creatorName,
+    required String recipeName,
+    required String cremeLogoMessage,
+    required BuildContext context,
+    required String? backgroundVideoUrl,
+    Color backgroundColor = Colors.grey,
+    double textScaleFactor = 1.2,
+    String? contentURL,
+  }) async {
+    final devicePixelRatio =
+        max(3, MediaQuery.of(context).devicePixelRatio).toDouble();
+    final screenSize = MediaQuery.of(context).size;
+    final safeAreaPadding = MediaQuery.of(context).padding;
+    final maxHeight = screenSize.height - safeAreaPadding.vertical - 48;
+    final maxWidth = screenSize.width;
+    const avatarSize = Size(40, 40);
+    const cookedImageSize = Size(261, 261);
+    final imageBackgroundSize = Size(maxWidth, maxHeight);
+    final hasVideoOnBackground = backgroundVideoUrl != null;
+    final imageBackgroundUrlToDownload = '$creatorAvatarUrl'
+        '?w=${(devicePixelRatio * imageBackgroundSize.width).round()}'
+        '&h=${(devicePixelRatio * imageBackgroundSize.width).round()}';
+    final creatorAvatarUrlToDownload = '$creatorAvatarUrl'
+        '?w=${(devicePixelRatio * avatarSize.width).round()}'
+        '&h=${(devicePixelRatio * avatarSize.width).round()}';
+    final cookedImageUrlToDownload = '$cookedImageUrl'
+        '?w=${(devicePixelRatio * cookedImageSize.width).round()}'
+        '&h=${(devicePixelRatio * cookedImageSize.height).round()}';
+    final userAvatarUrlToDownload = userAvatarUrl != null
+        ? '$userAvatarUrl'
+            '?w=${(devicePixelRatio * avatarSize.width).round()}'
+            '&h=${(devicePixelRatio * avatarSize.width).round()}'
+        : null;
+    final files = await Future.wait([
+      _downloadFile(
+        url: creatorAvatarUrlToDownload,
+        filename: 'creator_avatar.png',
+      ),
+      _downloadFile(
+        url: cookedImageUrlToDownload,
+        filename: 'cooked_image.png',
+      ),
+      if (userAvatarUrlToDownload != null)
+        _downloadFile(
+          url: userAvatarUrlToDownload,
+          filename: 'user_avatar.png',
+        ),
+      if (!hasVideoOnBackground)
+        _downloadFile(
+          url: imageBackgroundUrlToDownload,
+          filename: 'image_background.png',
+        ),
+    ]);
+    final stickerImagePngBytes = await _screenshotController.captureFromWidget(
+      CookedStickerWidget(
+        textScaleFactor: textScaleFactor,
+        hasVideoOnBackground: hasVideoOnBackground,
+        imageBackgroundFile: files.length >= 3 ? files.last : null,
+        creatorAvatarFile: files[0]!,
+        cookedImageFile: files[1]!,
+        userAvatarFile: files.length >= 3 && userAvatarUrlToDownload != null
+            ? files[2]
+            : null,
+        cookedImageSize: cookedImageSize,
+        imageBackgroundSize: imageBackgroundSize,
+        userAvatarSize: avatarSize,
+        creatorAvatarSize: avatarSize,
+        userName: userName,
+        recipeName: recipeName,
+        backgroundColor: backgroundColor,
+        creatorName: creatorName,
+        cremeLogoMessage: cremeLogoMessage,
+        devicePixelRatio: devicePixelRatio,
+      ),
+      pixelRatio: devicePixelRatio,
+      context: context,
     );
+    await Future.wait([
+      ...files.map<Future>((file) async {
+        final exists = (await file?.exists()) ?? false;
+        if (exists) {
+          await file?.delete();
+        }
+      }).toList(),
+      shareToInstagramStories(
+        backgroundImage:
+            hasVideoOnBackground ? null : base64Encode(stickerImagePngBytes),
+        stickerImage:
+            hasVideoOnBackground ? base64Encode(stickerImagePngBytes) : null,
+        backgroundTopColor: backgroundColor,
+        backgroundBottomColor: backgroundColor,
+        contentURL: contentURL,
+        backgroundVideo: backgroundVideoUrl,
+      ),
+    ]);
+  }
+
+  Future<File?> _downloadFile({
+    required String url,
+    required String filename,
+  }) async {
+    try {
+      final request = await _httpClient.getUrl(Uri.parse(url));
+      final response = await request.close();
+      final bytes = await consolidateHttpClientResponseBytes(
+        response,
+        autoUncompress: true,
+      );
+      final temporaryDirectory = (await getTemporaryDirectory()).path;
+      final file = File('$temporaryDirectory/$filename');
+      return await file.writeAsBytes(bytes);
+    } catch (err) {
+      return null;
+    }
   }
 }
--- lib/utils/creator_sticker_widget.dart
+++ lib/utils/creator_sticker_widget.dart
@@ -1,4 +1,4 @@
-import 'dart:typed_data';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -8,127 +8,141 @@ import 'package:creme_sharing/assets/fonts/app_fonts.dart';
 import 'package:creme_sharing/assets/svg/app_svg.dart';
 
 class CreatorStickerWidget extends StatelessWidget {
-  final Uint8List creatorAvatarImage;
+  final File? imageBackgroundFile;
+  final File creatorAvatarFile;
+  final Size extraRecipesImageSize;
   final String creatorName;
-  final String creatorTag;
   final String cremeLogoMessage;
-  final Color backgroundColor;
+  final Color? backgroundColor;
+  final Size creatorAvatarSize;
+  final Size imageBackgroundSize;
+  final double devicePixelRatio;
+  final bool hasVideoOnBackground;
+  final double? textScaleFactor;
+  final String creatorTag;
 
   const CreatorStickerWidget({
     Key? key,
-    required this.creatorAvatarImage,
+    required this.extraRecipesImageSize,
+    required this.textScaleFactor,
+    required this.creatorAvatarFile,
+    required this.imageBackgroundFile,
     required this.creatorName,
-    required this.creatorTag,
     required this.cremeLogoMessage,
     required this.backgroundColor,
+    required this.creatorAvatarSize,
+    required this.imageBackgroundSize,
+    required this.devicePixelRatio,
+    required this.hasVideoOnBackground,
+    required this.creatorTag,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: backgroundColor,
-      body: Stack(
+    const packageName = 'creme_sharing';
+    return Container(
+      color: hasVideoOnBackground
+          ? Colors.transparent
+          : backgroundColor ?? Colors.transparent,
+      child: Stack(
         children: [
-          Positioned.fill(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(24),
-              child: Image.memory(
-                creatorAvatarImage,
-                fit: BoxFit.cover,
+          if (!hasVideoOnBackground && imageBackgroundFile != null)
+            Positioned.fill(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(24),
+                child: Image.file(
+                  imageBackgroundFile!,
+                  fit: BoxFit.cover,
+                  height: imageBackgroundSize.height,
+                  width: imageBackgroundSize.width,
+                  filterQuality: FilterQuality.high,
+                ),
               ),
             ),
-          ),
-          Positioned.fill(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(24),
-              child: Container(
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      Colors.transparent,
-                      Colors.black87,
-                    ],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
+          if (!hasVideoOnBackground && imageBackgroundFile != null)
+            Positioned.fill(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(24),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.transparent,
+                        Colors.black87,
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
           Positioned.fill(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
                 children: [
                   const Spacer(),
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.15),
-                        SizedBox(
-                          height: 40,
-                          width: 40,
-                          child: ClipOval(
-                            child: Image.memory(
-                              creatorAvatarImage,
-                              height: 40,
-                              width: 40,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 8),
-                        Text(
-                          creatorName,
-                          style:
-                              AppFonts.bodyBold.copyWith(color: Colors.white),
-                          textAlign: TextAlign.center,
-                          textScaleFactor:
-                              MediaQuery.of(context).textScaleFactor,
-                        ),
-                        Text(
-                          creatorTag,
-                          style: AppFonts.bodyRegular
-                              .copyWith(color: AppColors.white50),
-                          textAlign: TextAlign.center,
-                          textScaleFactor:
-                              MediaQuery.of(context).textScaleFactor,
-                        ),
-                        const SizedBox(height: 34),
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Spacer(),
-                            Text(
-                              cremeLogoMessage,
-                              style: AppFonts.bodyRegular
-                                  .copyWith(color: AppColors.white50),
-                              textAlign: TextAlign.center,
-                              textScaleFactor:
-                                  MediaQuery.of(context).textScaleFactor,
-                            ),
-                            const SizedBox(width: 4),
-                            SvgPicture.asset(
-                              AppSvg.cremeLogo,
-                              package: 'creme_sharing',
-                              height: 29,
-                              width: 35,
-                              fit: BoxFit.fill,
-                            ),
-                            const Spacer(),
-                          ],
-                        ),
-                        const SizedBox(height: 32),
-                        const Spacer(),
-                      ],
+                  const SizedBox(height: 16),
+                  SizedBox(
+                    height: creatorAvatarSize.height,
+                    width: creatorAvatarSize.width,
+                    child: ClipOval(
+                      child: Image.file(
+                        creatorAvatarFile,
+                        height: creatorAvatarSize.height,
+                        width: creatorAvatarSize.width,
+                        fit: BoxFit.cover,
+                        filterQuality: FilterQuality.high,
+                      ),
                     ),
                   ),
+                  const SizedBox(height: 8),
+                  Text(
+                    creatorName,
+                    style: AppFonts.bodyBold.copyWith(color: Colors.white),
+                    textAlign: TextAlign.center,
+                    textScaleFactor: textScaleFactor ??
+                        MediaQuery.of(context).textScaleFactor,
+                  ),
+                  Text(
+                    creatorTag,
+                    style:
+                        AppFonts.bodyRegular.copyWith(color: AppColors.white50),
+                    textAlign: TextAlign.center,
+                    textScaleFactor: textScaleFactor ??
+                        MediaQuery.of(context).textScaleFactor,
+                  ),
+                  const SizedBox(height: 78),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Spacer(),
+                      Text(
+                        cremeLogoMessage,
+                        style: AppFonts.bodyRegular
+                            .copyWith(color: AppColors.white50),
+                        textAlign: TextAlign.center,
+                        textScaleFactor: textScaleFactor ??
+                            MediaQuery.of(context).textScaleFactor,
+                      ),
+                      const SizedBox(width: 4),
+                      SvgPicture.asset(
+                        AppSvg.cremeLogo,
+                        package: packageName,
+                        height: 29,
+                        width: 35,
+                        fit: BoxFit.fill,
+                      ),
+                      const Spacer(),
+                    ],
+                  ),
+                  const SizedBox(height: 134),
                 ],
               ),
             ),
